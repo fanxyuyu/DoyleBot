a377f6ae959ef6ef34202ca187acc41691e769bc
fanxyuyu <69539239+fanxyuyu@users.noreply.github.com>
2020-09-12 18:15:28 -0300

    Update help.py

--- bot/cogs/help.py
+++ bot/cogs/help.py
@@ -6,31 +6,33 @@ class help(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-#custom help command
+    #custom help command
     @commands.command()
     async def help(self, ctx):
         embed = discord.Embed(
             title = 'Commands list',
             colour = discord.Colour.blue()
             )
-        embed.set_thumbnail(url='https://i.ibb.co/S7mKyfQ/123.jpg')
-        embed.add_field(name='Admin', value='clear')
+        embed.set_footer(text='*The moon is friend for the lonesome to talk to.*')
+        #embed.set_thumbnail(url='https://i.ibb.co/S7mKyfQ/123.jpg')
+        embed.set_image(url='https://i.ibb.co/S7mKyfQ/123.jpg')
+        embed.add_field(name='Admin/Fun', value='clear (A)\n8ball\nnames\nreverse')
+        embed.add_field(name='Anime', value='anime\nsenpai\nhug')
         embed.add_field(name='Basic', value='help\nping\nitaly')
-        embed.add_field(name='Fun', value='8ball\nnames\nanime\nreverse', inline=False)
         await ctx.send(embed=embed)
 
-#names commands
+    #names commands
     @commands.command()
     async def names(self, ctx):
         embed = discord.Embed(colour = discord.Colour.blue())
         embed.add_field(name='Names', value='baruel\nbruno\ncat\ndaniel\nfelipe\ngiovanna\nhiter\nleo\npv\nwilly', inline=False)
         await ctx.send(embed=embed)
 
-#anime commands
+    #anime commands
     @commands.command()
     async def anime(self, ctx):
         embed = discord.Embed(colour = discord.Colour.blue())
-        embed.add_field(name='Commands', value='senpai\nhug', inline=False)
+        embed.add_field(name='Commands', value='**senpai:** *<@user>* notice me!\n**hug:** *<@user>* hugs someone', inline=False)
         await ctx.send(embed=embed)
 
 def setup(client):
