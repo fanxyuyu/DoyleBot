054f3410fe6130daa2794e09cafb220c36693bbd
fanxyuyu <69539239+fanxyuyu@users.noreply.github.com>
2020-09-12 23:34:46 -0300

    Update fun.py

--- bot/cogs/fun.py
+++ bot/cogs/fun.py
@@ -7,6 +7,22 @@ class fun(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    #say as the bot
+    @commands.command() #use to set a list that can run a command
+    async def text(self, ctx, embtext): #you can't start a command with number
+        await ctx.channel.purge(limit = 1)
+        await ctx.send(f'{embtext}')
+        return
+
+    #Embed text
+    @commands.command() #use to set a list that can run a command
+    async def embed(self, ctx, embtext): #you can't start a command with number
+        await ctx.channel.purge(limit = 1)
+        embed = discord.Embed(description = f"{embtext}", colour = discord.Colour.blue())
+        await ctx.send(embed=embed)
+        return
+
+    #reverses the text
     @commands.command()
     async def reverse(self, ctx, *, text: str):
         t_rev = text[::-1].replace("@", "@\u200B").replace("&", "&\u200B")
